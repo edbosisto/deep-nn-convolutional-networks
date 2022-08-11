3752331b4c8d2309328449a7a611132fa3fb3ffc
edbosisto <e.bosisto@gmail.com>
2022-08-11 16:28:33 +0200

    loading images and training step function

--- 3-face-recognition/neural-style-transfer.py
+++ 3-face-recognition/neural-style-transfer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib as plt
 
 
 # Implement Neural Style transfer to create your own artwork. 
@@ -165,5 +166,78 @@ def tensor_to_image(tensor):
     return Image.fromarray(tensor)
 
 
+# Load content image
+content_image = np.array(Image.open("images/louvre_small.jpg").resize((img_size, img_size)))
+content_image = tf.constant(np.reshape(content_image, ((1,) + content_image.shape)))
 
+print(content_image.shape)
+imshow(content_image[0])
+plt.show()
+
+# Load style image
+style_image =  np.array(Image.open("images/monet.jpg").resize((img_size, img_size)))
+style_image = tf.constant(np.reshape(style_image, ((1,) + style_image.shape)))
+
+print(style_image.shape)
+imshow(style_image[0])
+plt.show()
+
+# Randomly initialize the image to be generated
+generated_image = tf.Variable(tf.image.convert_image_dtype(content_image, tf.float32))
+noise = tf.random.uniform(tf.shape(generated_image), -0.25, 0.25)
+generated_image = tf.add(generated_image, noise)
+generated_image = tf.clip_by_value(generated_image, clip_value_min=0.0, clip_value_max=1.0)
+
+print(generated_image.shape)
+imshow(generated_image.numpy()[0])
+plt.show()
+
+
+# Load a pre trained model
+def get_layer_outputs(vgg, layer_names):
+    """ Creates a vgg model that returns a list of intermediate output values."""
+    outputs = [vgg.get_layer(layer[0]).output for layer in layer_names]
+
+    model = tf.keras.Model([vgg.input], outputs)
+    return model
+
+content_layer = [('block5_conv4', 1)]
+
+vgg_model_outputs = get_layer_outputs(vgg, STYLE_LAYERS + content_layer)
+
+content_target = vgg_model_outputs(content_image)  # Content encoder
+style_targets = vgg_model_outputs(style_image)     # Style enconder
+
+
+# Training step
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+
+def train_step(generated_image):
+    with tf.GradientTape() as tape:
+        # In this function you must use the precomputed encoded images a_S and a_C
+        # Compute a_G as the vgg_model_outputs for the current generated image
+        
+        ### START CODE HERE
+        
+        #(1 line)
+        a_G = vgg_model_outputs(generated_image)
+        
+        # Compute the style cost
+        #(1 line)
+        J_style = compute_style_cost(a_S, a_G)
+
+        #(2 lines)
+        # Compute the content cost
+        J_content = compute_content_cost(a_C, a_G)
+        # Compute the total cost
+        J = total_cost(J_content, J_style, alpha=10, beta=40)
+        
+        ### END CODE HERE
+        
+    grad = tape.gradient(J, generated_image)
+
+    optimizer.apply_gradients([(grad, generated_image)])
+    generated_image.assign(clip_0_1(generated_image))
+    # For grading purposes
+    return J
 
