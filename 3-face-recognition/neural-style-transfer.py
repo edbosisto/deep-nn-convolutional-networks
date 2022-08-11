aee9e0f18b1fb727a294060dbc5047c72bbed696
edbosisto <e.bosisto@gmail.com>
2022-08-11 16:09:43 +0200

    compute layer style cost function

--- 3-face-recognition/neural-style-transfer.py
+++ 3-face-recognition/neural-style-transfer.py
@@ -48,5 +48,34 @@ def gram_matrix(A):
     return GA
 
 
+# Compute layer style cost
+def compute_layer_style_cost(a_S, a_G):
+    """
+    Arguments:
+    a_S -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image S 
+    a_G -- tensor of dimension (1, n_H, n_W, n_C), hidden layer activations representing style of the image G
+    
+    Returns: 
+    J_style_layer -- tensor representing a scalar value, style cost defined above by equation (2)
+    """
+    
+    # Retrieve dimensions from a_G 
+    _, n_H, n_W, n_C = a_G.get_shape().as_list()
+    
+    # Reshape the images from (n_H * n_W, n_C) to have them of shape (n_C, n_H * n_W) 
+    a_S = tf.transpose(tf.reshape(a_S, shape=[-1, n_C]))
+    a_G = tf.transpose(tf.reshape(a_G, shape=[-1, n_C]))
+
+    # Computing gram_matrices for both images S and G 
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+
+    # Computing the loss 
+    J_style_layer = 1/(4*n_H*n_W*n_C)*(tf.reduce_sum(tf.square(tf.subtract(GS, GG))))
+    J_style_layer = 1/(4.0 *(( n_H * n_W * n_C)**2)) * tf.reduce_sum(tf.square(GS - GG))
+    
+    return J_style_layer
+
+
 
 
