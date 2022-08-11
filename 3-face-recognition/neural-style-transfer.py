2e698f6aab950ea0ce1d802dd2557227f230e950
edbosisto <e.bosisto@gmail.com>
2022-08-11 16:29:40 +0200

    small changes train step

--- 3-face-recognition/neural-style-transfer.py
+++ 3-face-recognition/neural-style-transfer.py
@@ -209,35 +209,24 @@ content_target = vgg_model_outputs(content_image)  # Content encoder
 style_targets = vgg_model_outputs(style_image)     # Style enconder
 
 
-# Training step
+# Train step
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
 def train_step(generated_image):
     with tf.GradientTape() as tape:
         # In this function you must use the precomputed encoded images a_S and a_C
         # Compute a_G as the vgg_model_outputs for the current generated image
-        
-        ### START CODE HERE
-        
-        #(1 line)
         a_G = vgg_model_outputs(generated_image)
-        
         # Compute the style cost
-        #(1 line)
         J_style = compute_style_cost(a_S, a_G)
-
-        #(2 lines)
         # Compute the content cost
         J_content = compute_content_cost(a_C, a_G)
         # Compute the total cost
         J = total_cost(J_content, J_style, alpha=10, beta=40)
         
-        ### END CODE HERE
-        
     grad = tape.gradient(J, generated_image)
 
     optimizer.apply_gradients([(grad, generated_image)])
-    generated_image.assign(clip_0_1(generated_image))
-    # For grading purposes
+    # generated_image.assign(clip_0_1(generated_image))
     return J
 
